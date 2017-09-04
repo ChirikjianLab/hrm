1b079ae838b7cd83615e8ec0cc56a2c782b063ef
Qianli Ma <mqianli1@jhu.edu>
2017-09-04 18:52:06 -0400

    converted data from Eigen::Matrix to std double

--- test/test.cpp
+++ test/test.cpp
@@ -7,16 +7,26 @@
 
 using namespace Eigen;
 using namespace std;
+
 #define pi 3.1415926
+#define w 100
 
 int main(){
-  MatrixXd X_eb;
+  MatrixXd matX_eb;
+  double *X_eb;
+
   double a[6] = {5,3,0,0.5,0,0};
   double b[3] = {2,1,pi/6};
   int num = 50, K = 1;
 
-  X_eb = minkSum2D(a, b, num, K);
-  //cout << X_eb << endl;
+  matX_eb = minkSum2D(a, b, num, K);
+
+  X_eb = matX_eb.data();
+
+  //Map<MatrixXd>(X_eb, matX_eb.rows(), matX_eb.cols()) = matX_eb;
+
+  for(int i = 0; i < num; i++)
+  cout << X_eb[i] << ',';
 
   return 0;
 }
