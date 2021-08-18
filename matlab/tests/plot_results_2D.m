1c47aaace385e7d75208eff0169f1a7de1b4343e
Sipu Ruan <ruansp@nus.edu.sg>
2021-08-18 22:51:20 +0800

    Minor modifications for Matlab visualization scripts

--- matlab/tests/plot_results_2D.m
+++ matlab/tests/plot_results_2D.m
@@ -29,42 +29,7 @@ end
 %     plot(X(i,:),X(i+1,:),'k.');
 % end
 
-%% raster scan
-% N_dy = size(X_obs)/2;
-% % obs
-% for i = 1:size(X_obs,2)
-%     plot(X_obs(1:N_dy,i),Y,'r.')
-%     plot(X_obs(N_dy+1:N_dy*2,i),Y,'b.')
-% end
-%
-% % exploded obs
-% for i = 1:size(X_obs_ex,2)
-%     plot(X_obs_ex(1:N_dy,i),Y,'r*')
-%     plot(X_obs_ex(N_dy+1:N_dy*2,i),Y,'b*')
-% end
-%
-% % arena
-% for i = 1:size(X_arena,2)
-%     plot(X_arena(1:N_dy,i),Y,'.')
-%     plot(X_arena(N_dy+1:N_dy*2,i),Y,'.')
-% end
-%
-% % cells
-% for i = 1:size(cf_seg,1)
-%     plot([cf_seg(i,2),cf_seg(i,4)], [cf_seg(i,1), cf_seg(i,1)], 'g');
-%         plot(cf_seg(i,3), cf_seg(i,1),'k*')
-% end
-%
-% % vertex and connections
-% plot3(vtx(:,1), vtx(:,2), sc*vtx(:,3),'k.');
-% edge = edge+1;
-% for i = 1:size(edge,1)
-%     plot3([vtx(edge(i,1),1) vtx(edge(i,2),1)],...
-%         [vtx(edge(i,1),2) vtx(edge(i,2),2)],...
-%         sc*[vtx(edge(i,1),3) vtx(edge(i,2),3)], 'k')
-% end
-
-% Robot motions
+%% Robot motions
 rob = MultiBodyTree2D(SuperEllipse([robot(1,1:6), 50], 'g', 0),...
     size(robot,1)-1);
 for i = 1:size(robot,1)-1
@@ -152,38 +117,3 @@ for i = 1:numel(path)
     pathHRM = [pathHRM; vtx(path(i)+1,:)];
 end
 pathHRM = [pathHRM; goal'];
-
-% csvwrite('pathCpp.csv', pathHRM);
-
-%% Path for PRM
-% path_prm = load([loadPath, 'prm_path.csv']);
-% state_prm = load([loadPath, 'prm_state.csv']);
-% edge_prm = load([loadPath, 'prm_edge.csv']);
-%
-% path_prm(:,3) = path_prm(:,3)+pi;
-% state_prm(:,3) = state_prm(:,3)+pi;
-%
-% plot3([start(1) path_prm(1,1)],...
-%     [start(2) path_prm(1,2)],...
-%     sc*[start(3) path_prm(1,3)], 'r', 'LineWidth', 2)
-% plot3([goal(1) path_prm(end,1)],...
-%     [goal(2) path_prm(end,2)],...
-%     sc*[goal(3) path_prm(end,3)], 'g', 'LineWidth', 2)
-%
-% for i = 1:size(path_prm,1)-1
-%     plot3([path_prm(i,1) path_prm(i+1,1)],...
-%         [path_prm(i,2) path_prm(i+1,2)],...
-%         sc*[path_prm(i,3) path_prm(i+1,3)], 'b-', 'LineWidth', 2)
-% end
-%
-% for i = 1:size(state_prm,1)-1
-%     plot3(state_prm(i,1),state_prm(i,2),sc*state_prm(i,3),...
-%         'b+', 'LineWidth', 2)
-% end
-%
-% for i = 1:size(edge_prm,1)-1
-%     plot3([state_prm(edge_prm(i,1)+1,1) state_prm(edge_prm(i,2)+1,1)],...
-%         [state_prm(edge_prm(i,1)+1,2) state_prm(edge_prm(i,2)+1,2)],...
-%         sc*[state_prm(edge_prm(i,1)+1,3) state_prm(edge_prm(i,2)+1,3)],...
-%         'b--', 'LineWidth', 2)
-% end
