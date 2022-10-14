d99dd5dabeb2b059146d7074a2c240095ef6b8a4
Sipu Ruan <ruansp@nus.edu.sg>
2022-10-14 10:32:36 +0800

    Minor change

--- result/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define BENCHMARK_DATA_PATH "/home/ruan/research_ws/HighwayRoadMap/result/benchmark"
-#define SOLUTION_DETAILS_PATH "/home/ruan/research_ws/HighwayRoadMap/result/details"
