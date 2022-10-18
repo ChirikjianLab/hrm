54e8347cc2cdfac9c480c0cd8e4dae67c0fa8997
Sipu Ruan <ruansp@nus.edu.sg>
2022-10-18 20:28:36 +0800

    Use cgal 5.5.1 in dependency script

--- script/install-dependencies-on-localhost.sh
+++ script/install-dependencies-on-localhost.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ `id -u` == 0 ]; then
+    SUDO=
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get -y install lsb-release
+else
+    SUDO="sudo -H"
+fi
+
 if [[ -d "$HOME"/tmpHRM ]]; then
   echo "tmpHRM exists already, skip creating it"
 else
@@ -12,19 +20,19 @@ fi
 buildAndInstall() {
   mkdir build && cd build || exit
   cmake -G Ninja ../"$1"
-  ninja && sudo ninja install
+  ninja && ${SUDO} ninja install
   cd .. && rm -rf build && rm -rf "$1"
 }
 
 
 # Build essentials
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install g++ cmake pkg-config libeigen3-dev libboost-dev libgtest-dev ninja-build wget
+${SUDO} apt -y update
+${SUDO} apt -y upgrade
+${SUDO} apt -y install g++ cmake pkg-config libeigen3-dev libboost-dev libboost-filesystem-dev libgtest-dev ninja-build wget git
 export CXX=g++
 
 # Install dependency from deb packages manager
-sudo apt -y install libccd-dev libgmp-dev libmpfr-dev libcgal-dev liburdfdom-dev libtinyxml2-dev
+${SUDO} apt -y install libccd-dev libgmp-dev libmpfr-dev libcgal-dev liburdfdom-dev libtinyxml2-dev
 
 # Install dependencies from source
 cd "$HOME"/tmpHRM || exit
@@ -39,6 +47,11 @@ wget -O - https://github.com/ompl/ompl/archive/refs/tags/1.5.2.tar.gz | tar zxf 
 srcDir="ompl-1.5.2"
 buildAndInstall "$srcDir"
 
+# cgal 5.5.1
+wget -O - https://github.com/CGAL/cgal/archive/refs/tags/v5.5.1.tar.gz | tar zxf -
+srcDir="cgal-5.5.1"
+buildAndInstall "$srcDir"
+
 # kdl build and install
 git clone https://github.com/orocos/orocos_kinematics_dynamics.git
 srcDir="orocos_kinematics_dynamics"
@@ -47,4 +60,4 @@ srcDir="orocos_kdl"
 buildAndInstall "$srcDir"
 cd "$HOME"/tmpHRM || exit
 
-sudo rm -r "$HOME"/tmpHRM
+${SUDO} rm -r "$HOME"/tmpHRM
