ebfb2a833ef187ba3b7c2bc5f5e0aac7f8f143eb
Sipu Ruan <ruansp@nus.edu.sg>
2022-10-18 14:00:40 +0800

    Update install-dependencies-on-localhost.sh

--- script/install-dependencies-on-localhost.sh
+++ script/install-dependencies-on-localhost.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Note: Use this script to install dependencies on localhost if you want to build everything locally.
-# No guarantee to work and recommend using the provided docker image to run and develop
-
-# stop execution instantly. Also print the error location of the running code.
 set -e
 
-eval "$(ssh-agent -s)"
-ssh-add "$HOME"/.ssh/id_ed25519_personal
-
-# Use ninja build system for fast speed
-sudo apt-get install -y ninja-build
-
-mkdir "$HOME"/tmpHRM
-cd "$HOME"/tmpHRM || exit
+if [[ -d "$HOME"/tmpHRM ]]; then
+  echo "tmpHRM exists already, skip creating it"
+else
+  echo "creating tmpHRM folder"
+  mkdir "$HOME"/tmpHRM
+  cd "$HOME"/tmpHRM || exit
+fi
 
 buildAndInstall() {
   mkdir build && cd build || exit
@@ -22,27 +17,26 @@ buildAndInstall() {
   cd .. && rm -rf build && rm -rf "$1"
 }
 
-# Depend on Boost 1.71.0 as the default of Ubuntu 20.04
 
-# libccd install from source
-git clone https://github.com/danfis/libccd.git
-srcDir="libccd"
-buildAndInstall "$srcDir"
+# Build essentials
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install g++ cmake pkg-config libeigen3-dev libboost-dev libgtest-dev ninja-build
+export CXX=g++
 
-# fcl 0.6 install from source
+# Install dependency from deb packages manager
+sudo apt -y install libccd-dev libgmp-dev libmpfr-dev libompl-dev iburdfdom-dev libtinyxml2-dev
+
+# Install dependencies from source
+# fcl 0.6
 git clone https://github.com/flexible-collision-library/fcl.git
 srcDir="fcl"
 buildAndInstall "$srcDir"
 
-# cgal 5.2 install from source
-sudo apt install libgmp-dev
-
+# cgal 5.2
 git clone https://github.com/CGAL/cgal.git
 srcDir="cgal"
 buildAndInstall "$srcDir"
-
-# ompl install
-sudo apt-get install libompl-dev ompl-demos
 
 # kdl build and install
 git clone https://github.com/orocos/orocos_kinematics_dynamics.git
@@ -51,13 +45,5 @@ cd "$srcDir" || exit
 srcDir="orocos_kdl"
 buildAndInstall "$srcDir"
 cd "$HOME"/tmpHRM || exit
-
-# gtest build and install
-git clone https://github.com/google/googletest.git -b release-1.10.0
-srcDir="googletest"
-buildAndInstall "$srcDir"
-
-# install deb package for urdfdom and tinmyxml2
-sudo apt install liburdfdom-dev libtinyxml2-dev
 
 sudo rm -r "$HOME"/tmpHRM
