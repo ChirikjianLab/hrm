c247470158c50e098b2bd53ff3ae7332dcaf68e7
raghav <rsnavaratna@gmail.com>
2023-06-28 16:32:12 +0800

    Numpy 1.20 np.float deprecated - updated

--- demo/python/result_loader.py
+++ demo/python/result_loader.py
@@ -8,12 +8,12 @@ import numpy as np
 
 def load_planning_scene(dim, config_path):
     # Planning environment
-    arena_config = pd.read_csv(config_path + "arena_config_" + dim + ".csv", header=None, dtype=np.float).values
-    obstacle_config = pd.read_csv(config_path + "obstacle_config_" + dim + ".csv", header=None, dtype=np.float).values
+    arena_config = pd.read_csv(config_path + "arena_config_" + dim + ".csv", header=None, dtype=np.float64).values
+    obstacle_config = pd.read_csv(config_path + "obstacle_config_" + dim + ".csv", header=None, dtype=np.float64).values
 
     # Robot configurations
-    robot_config = pd.read_csv(config_path + "robot_config_" + dim + ".csv", header=None, dtype=np.float).values
-    end_pts = pd.read_csv(config_path + "end_points_" + dim + ".csv", header=None, dtype=np.float).values
+    robot_config = pd.read_csv(config_path + "robot_config_" + dim + ".csv", header=None, dtype=np.float64).values
+    end_pts = pd.read_csv(config_path + "end_points_" + dim + ".csv", header=None, dtype=np.float64).values
 
     return robot_config, arena_config, obstacle_config, end_pts
 
@@ -21,10 +21,10 @@ def load_planning_scene(dim, config_path):
 def load_results(suffix, result_path):
     try:
         x_origin = pd.read_csv(result_path + "origin_bound_" + suffix + ".csv",
-                               delimiter='\s+', header=None, dtype=np.float).values
+                               delimiter='\s+', header=None, dtype=np.float64).values
         x_mink = pd.read_csv(result_path + "mink_bound_" + suffix + ".csv",
-                             delimiter='\s+', header=None, dtype=np.float).values
-        cf_seg = pd.read_csv(result_path + "segment_" + suffix + ".csv", header=None, dtype=np.float).values
+                             delimiter='\s+', header=None, dtype=np.float64).values
+        cf_seg = pd.read_csv(result_path + "segment_" + suffix + ".csv", header=None, dtype=np.float64).values
     except:
         x_origin = None
         x_mink = None
@@ -32,12 +32,12 @@ def load_results(suffix, result_path):
 
         print('No discrete points on obstacles will be shown.')
 
-    vtx = pd.read_csv(result_path + "vertex_" + suffix + ".csv", header=None, dtype=np.float).values
-    edge = pd.read_csv(result_path + "edge_" + suffix + ".csv", header=None, dtype=np.float).values
+    vtx = pd.read_csv(result_path + "vertex_" + suffix + ".csv", header=None, dtype=np.float64).values
+    edge = pd.read_csv(result_path + "edge_" + suffix + ".csv", header=None, dtype=np.float64).values
 
     try:
-        path = pd.read_csv(result_path + "interpolated_path_" + suffix + ".csv", header=None, dtype=np.float).values
+        path = pd.read_csv(result_path + "interpolated_path_" + suffix + ".csv", header=None, dtype=np.float64).values
     except:
-        path = pd.read_csv(result_path + "solution_path_" + suffix + ".csv", header=None, dtype=np.float).values
+        path = pd.read_csv(result_path + "solution_path_" + suffix + ".csv", header=None, dtype=np.float64).values
 
     return x_origin, x_mink, cf_seg, vtx, edge, path
